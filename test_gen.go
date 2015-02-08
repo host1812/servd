14e500f62a950bd2e9498b32c3dbade011e5b6c5
host1812 <host1812@yandex.ru>
2015-02-07 23:31:58 -0800

    test_gen now can be run with params

--- test_gen.go
+++ test_gen.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"servd/generator"
+	"strconv"
 )
 
 func main() {
-	generator.Generate("/usr/share/dict/words", 50, 10)
+	args := os.Args[1:]
+	pcount := 50
+	wcount := 50
+
+	if len(args) >= 1 {
+		pcount, _ = strconv.Atoi(args[0])
+	}
+
+	if len(args) >= 2 {
+		wcount, _ = strconv.Atoi(args[1])
+	}
+
+	fmt.Println("pcount = ", pcount)
+	fmt.Println("wcount = ", wcount)
+	generator.Generate("/usr/share/dict/words", pcount, wcount)
 }
